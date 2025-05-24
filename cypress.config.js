b158156fd274bed00fd52bdcaae8b8f2b24bc6bd
SANTHOSH DASARI <santhosh.dasari@accionlabs.com>
2025-05-24 18:45:29 +0530

    added yml file to show runs in dashboard

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    projectId: "ann8w1",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
